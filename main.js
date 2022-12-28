9d99be5001573b80d5b00aeef77b2a7cfb1ba9cb
Pedrick <pedrickantazo@gmail.com>
2022-12-28 18:49:33 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// const FirstName = "Pedrick"
+// const LastName = "Divinagracia"
+// const Age = 20
+// const Gender = "Male"
+
+// const identity = `My First Name is ${FirstName} and My Last Name is ${LastName} and My age is ${Age} And my Gender is ${Gender}`
+
+// console.log(identity)
+
+const Student = ["Pedrick","Divinagracia", 20,"Male"]
+
+
+// adding first to array
+// Student.unshift("Pogi")
+
+// adding Last to array
+// Student.push("Pogi")
+
+// removing to array with specific 
+// Student.splice(2,1)
+
+// removing to array on last 
+// Student.pop()
+
+// console.log(Gender.toLocaleLowerCase())
+
+//checking if array
+// console.log(Array.isArray(Student))
+
+//checking index
+// console.log(Student.indexOf(20))
+
+
+// https://youtu.be/AVTrIV_Qgt8
